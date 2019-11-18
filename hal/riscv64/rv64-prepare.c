9e6cd6677aaf1b08ede3150aec20ccd2da3367aa
Takeharu KATO <tkato@elliptic-curve.net>
2019-11-19 00:08:24 +0900

    修正概要: 物理ページプールの初期化。

--- hal/riscv64/rv64-prepare.c
+++ hal/riscv64/rv64-prepare.c
@@ -10,7 +10,9 @@
 #include <klib/freestanding.h>
 #include <kern/kern-common.h>
 #include <kern/spinlock.h>
+#include <kern/page-if.h>
 
+#include <hal/rv64-platform.h>
 #include <hal/hal-dbg-console.h>
 #include <hal/rv64-mscratch.h>
 
@@ -22,19 +24,55 @@ static spinlock prepare_lock=__SPINLOCK_INITIALIZER;
 
 int rv64_map_kernel_space(void);
 
+/**
+   物理メモリページプールの状態を表示
+ */
+static void
+show_memory_stat(void) {
+	pfdb_stat         st;
+
+	kcom_obtain_pfdb_stat(&st);
+
+	kprintf("nr_pages: %qu\n", st.nr_pages);
+	kprintf("nr_free_pages: %qu\n", st.nr_free_pages);
+	kprintf("reserved_pages: %qu\n", st.reserved_pages);
+	kprintf("available_pages: %qu\n", st.available_pages);
+	kprintf("kdata_pages: %qu\n", st.kdata_pages);
+	kprintf("kstack_pages: %qu\n", st.kstack_pages);
+	kprintf("pgtbl_pages: %qu\n", st.pgtbl_pages);
+	kprintf("slab_pages: %qu\n", st.slab_pages);
+	kprintf("anon_pages: %qu\n", st.anon_pages);
+	kprintf("pcache_pages: %qu\n", st.pcache_pages);
+}
+
 void
 prepare(uint64_t hartid){
 	intrflags iflags;
+	pfdb_ent   *pfdb;
 
-	if ( hartid == 0 )
+	if ( hartid == 0 ) {
+
 		hal_dbg_console_init();
-	spinlock_lock_disable_intr(&prepare_lock, &iflags);
-	kprintf("Boot on supervisor mode on %d hart\n", hartid);
-	spinlock_unlock_restore_intr(&prepare_lock, &iflags);
-	if ( hartid != 0 )
+
+		spinlock_lock_disable_intr(&prepare_lock, &iflags);
+		kprintf("Boot on supervisor mode on %d hart\n", hartid);
+		spinlock_unlock_restore_intr(&prepare_lock, &iflags);
+		kprintf("Add memory region [%p, %p) %ld MiB\n", 
+		    HAL_KERN_PHY_BASE, HAL_KERN_PHY_BASE + MIB_TO_BYTE(KC_PHYSMEM_MB),
+			KC_PHYSMEM_MB);
+		pfdb_add(HAL_KERN_PHY_BASE, MIB_TO_BYTE(KC_PHYSMEM_MB), &pfdb);
+		show_memory_stat();
+
+		rv64_map_kernel_space();
+		kern_init();
+	} else {
+
+		/* TODO: カーネル初期化完了を待ち合わせる */
+		spinlock_lock_disable_intr(&prepare_lock, &iflags);
+		kprintf("Boot on supervisor mode on %d hart\n", hartid);
+		spinlock_unlock_restore_intr(&prepare_lock, &iflags);
 		goto loop;
-	rv64_map_kernel_space();
-	kern_init();
+	}
 	kprintf("end\n");
 loop:
 	while(1);
--- include/kern/kern-consts.h
+++ include/kern/kern-consts.h
@@ -10,10 +10,16 @@
 #define  _KERN_KERN_CONSTS_H
 
 #include <kern/kern-autoconf.h>
+
 #define KC_KSTACK_SIZE (CONFIG_HAL_PAGE_SIZE * CONFIG_KSTACK_PAGE_NR)
 #if defined(CONFIG_SMP)
 #define KC_CPUS_NR (CONFIG_HAL_CPUS_NR)
 #else
 #define KC_CPUS_NR (1)
 #endif  /* CONFIG_SMP */
+#if defined(CONFIG_HAL_MEMORY_SIZE_MB)
+#define KC_PHYSMEM_MB (CONFIG_HAL_MEMORY_SIZE_MB)
+#else
+#error "CONFIG_HAL_MEMORY_SIZE_MB is not set."
+#endif  /*  CONFIG_HAL_MEMORY_SIZE_MB  */
 #endif  /* KERN_KERN_CONSTS_H */
