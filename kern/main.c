9380d2fdfdf5f66392c7b9ba61b7fb43aad84f38
Takeharu KATO <tkato@elliptic-curve.net>
2019-11-22 04:28:21 +0900

    修正概要: ユーザランドテスト用関数を追加。

--- kern/main.c
+++ kern/main.c
@@ -10,20 +10,31 @@
 #include <kern/kern-common.h>
 #include <kern/ktest.h>
 
-/** カーネルの初期化
+/** 
+    カーネルのアーキ共通テスト
  */
 void
-kern_init(void) {
+kern_common_tests(void){
 
 	tst_spinlock();
 	tst_atomic();
 	tst_atomic64();
 }
+/** カーネルの初期化
+ */
+void
+kern_init(void) {
+
+	kern_common_tests();
+}
 
 #if !defined(CONFIG_HAL)
 int 
 main(int argc, char *argv[]) {
+	
+	kprintf("Kernel\n");
 
+	kern_common_tests();
 
 	return 0;
 }
