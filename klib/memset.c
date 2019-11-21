a35aba6ebb97ce99315d4d6459d8d5219ceb7a7e
Takeharu KATO <tkato@elliptic-curve.net>
2019-11-22 03:44:04 +0900

    修正概要: memsetを改善。

--- klib/memset.c
+++ klib/memset.c
@@ -15,15 +15,56 @@
     @param[in] s 書き込み先メモリの先頭アドレス
     @param[in] c 書き込む値(charとして扱われる)
     @param[in] n 書き込むバイト数
+    @note 8バイトまたは4バイト境界から始まるアドレスについては, 可能な限り
+    8バイトまたは4バイト単位で埋め, 余りバイトはバイト単位で埋める。
+    OS内の典型的なデータ構造は, 8バイトまたは4バイト境界にそろっているので
+    典型的な場合に効果があり極力単純化した実装を採用している。
  */
 void *
 memset(void *s, int c, size_t n){
-	size_t len;
-	char *d;
+	uint64_t   val;
+	uint8_t   *cp8;
+	uint32_t *cp32;
+	uint64_t *cp64;
+	uint32_t   c32;
+	uint64_t   c64;
+	size_t     len;
 
-	for(d = (char *)s, len = n;len > 0;--len)
-		*d++ = (char)c;
+	len = n;  /* コピー長を記憶する */
+
+	/*  32bit, 64bit長のバイトcで埋められた領域を作成する  */
+	val = c & 0xff;  /* バイトcを符号なし64bit整数に変換  */
+	/*  32bitの領域をバイトcで埋める  */
+	c32 = (uint32_t)((val << 24) | (val << 16) | (val << 8) | val);
+	/*  64bitの領域をバイトcで埋める  */
+	c64 = ( ( (uint64_t)c32 ) << 32 ) | (uint64_t)c32;
+
+	if ( ( ( (uintptr_t)s & ( sizeof(uint64_t) - 1) ) == 0 ) 
+	    && ( len >= sizeof(uint64_t) ) ) {
+		
+		/* 8バイト境界で始まり, 残転送量が8バイト以上ある場合は, 8バイト単位で
+		 * 領域を埋める
+		 */
+		for( cp64 = s; len >= sizeof(uint64_t); len -= sizeof(uint64_t)) 
+			*cp64++ = c64;
+
+		cp8 = (uint8_t *)cp64;  /* 残りバイトの先頭を指す */
+	} else if ( ( ( (uintptr_t)s & ( sizeof(uint32_t) - 1) ) == 0 ) 
+	    && ( len >= sizeof(uint32_t) ) ) {
+
+		/* 4バイト境界で始まり, 残転送量が4バイト以上ある場合は, 4バイト単位で
+		 * 領域を埋める
+		 */
+		for( cp32 = s; len >= sizeof(uint32_t); len -= sizeof(uint32_t)) 
+			*cp32++ = c32;
+		
+		cp8 = (uint8_t *)cp32; /* 残りバイトの先頭を指す */
+	} else 
+		cp8 = (uint8_t *)s; /* 8バイトでも4バイト境界でもなければバイト単位で埋める */
+
+	/* 残りバイトをフィルする */
+	while ( len-- > 0 ) 
+		*cp8++ = (uint8_t)(c & 0xff);
 
 	return s;
 }
-
