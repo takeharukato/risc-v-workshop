b29cf7e18e46f1afe30d5532106499faee1b35bd
Takeharu KATO <tkato@elliptic-curve.net>
2019-12-17 01:45:48 +0900

    修正概要: フレームポインタ操作/スタックポインタ操作をアセンブラサブルーチンに置き換え。

--- include/hal-riscv64/hal-stack.h
+++ include/hal-riscv64/hal-stack.h
@@ -15,34 +15,8 @@
 #include <klib/freestanding.h>
 #include <kern/kern-common.h>
 
-/** ベースポインタ(フレームポインタ)を取得する(実装部)
-    @param[out] bp 現在の関数におけるベースポインタを返却する領域
- */
-static __always_inline void 
-hal_get_base_pointer(void **bp){
-
-	__asm__ __volatile__("mv %0, fp" : "=r" (*bp));  
-}
-
-/** スタックポインタを取得する(実装部)
-    @param[out] sp 現在の関数におけるスタックポインタを返却する領域
- */
-static __always_inline void 
-hal_get_stack_pointer(void **sp) {
-	
-	__asm__ __volatile__("mv %0, sp" : "=r" (*sp));  
-}
-
-/** スタックポインタを設定する(実装部)
-    @param[in] new_sp 設定する値
- */
-static __always_inline void 
-hal_set_stack_pointer(void *new_sp){
-
-	__asm__ __volatile__("mv sp, %0" : "=r" (new_sp));
-}
-
-void hal_call_with_newstack(void (*_func)(void *_argp), void *_argp, void *_new_sp,
-    void **old_sp);
+void hal_get_base_pointer(void **_bp);
+void hal_get_stack_pointer(void **_sp);
+void hal_set_stack_pointer(void *_new_sp);
 #endif  /* !ASM_FILE */
 #endif  /*  _HAL_STACK_H   */
