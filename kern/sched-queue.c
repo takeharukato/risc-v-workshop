024845782730bf26314149d0399e32e026576680
Takeharu KATO <tkato@elliptic-curve.net>
2019-12-18 18:09:31 +0900

    修正概要: スケジューラキュー途中まで。

--- kern/sched-queue.c
+++ kern/sched-queue.c
@@ -14,11 +14,34 @@
 #include <kern/sched-queue.h>
 
 static sched_queue ready_queue={.lock = __SPINLOCK_INITIALIZER,};
+
 /**
-   
+ */
+struct _thread *
+sched_find_next(void){
+	struct _thread  *thr;
+	singned_cnt_type idx;
+	intrflags     iflags;
+
+	spinlock_lock_disable_intr(&ready_queue.lock, &iflags);   /* レディキューをロック */
+
+	/* ビットマップの最初に立っているビットの位置を確認  */
+	idx = bitops_ffs(&ready_queue.bitmap);  
+	if ( idx == 0 )
+		goto unlock_out;  /* 実行可能なスレッドがない  */
+
+	spinlock_unlock_restore_intr(&ready_queue.lock, &iflags); /* レディキューをアンロック */
+	return thr;
+unlock_out:
+	spinlock_unlock_restore_intr(&ready_queue.lock, &iflags); /* レディキューをアンロック */
+	return NULL;
+}
+
+/**
+   スケジューラの初期化
  */
 void
-sched_schedqueue_init(void){
+sched_init(void){
 	int i;
 
 	spinlock_init(&ready_queue.lock);
