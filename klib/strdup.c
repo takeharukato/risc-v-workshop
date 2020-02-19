906e4bfc3afbfc0fabc1f2eb10a42328d292ded1
Takeharu KATO <tkato@elliptic-curve.net>
2020-02-19 22:09:34 +0900

    修正概要: strdupを追加。

---
+++ klib/strdup.c
@@ -0,0 +1,34 @@
+/* -*- mode: C; coding:utf-8 -*- */
+/**********************************************************************/
+/*  Yet Another Teachable Operating System                            */
+/*  Copyright 2016 Takeharu KATO                                      */
+/*                                                                    */
+/*  strdup routine                                                    */
+/*                                                                    */
+/**********************************************************************/
+
+#include <klib/freestanding.h>
+#include <kern/kern-common.h>
+#include <kern/page-if.h>
+
+/**
+   文字列の複製を返却する
+   @param[in] src  文字列
+   @return 文字列の複製
+ */
+char *
+kstrdup(const char *src){
+	size_t len;
+	void    *m;
+
+	len = strlen(src) + 1;  /* メモリ領域長を求める */
+
+	m = kmalloc(len, KMALLOC_NORMAL);
+	if ( m == NULL )
+		goto error_out;
+
+	strcpy(m, src);
+
+error_out:
+	return m;
+}
