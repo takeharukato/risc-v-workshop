44046cd8562626925cfc751170c9350e7fe31e9c
Takeharu KATO <tkato@elliptic-curve.net>
2020-03-10 12:04:02 +0900

    修正概要: RB木を定義。

--- test/tst-vfs-tstfs.c
+++ test/tst-vfs-tstfs.c
@@ -18,7 +18,123 @@
 static kmem_cache tstfs_super; /**< super blockのSLABキャッシュ */
 static kmem_cache tstfs_inode; /**< I-nodeのSLABキャッシュ */
 static kmem_cache tstfs_dent;  /**< ディレクトリエントリのSLABキャッシュ */
-static kmem_cache tstfs_data;  /**< データページのSLABキャッシュ */
+static kmem_cache tstfs_dpage; /**< データページのSLABキャッシュ */
+
+static tst_vfs_tstfs_db g_tstfs_db={.mtx = __MUTEX_INITIALIZER(&g_tstfs_db.mtx),\
+				    .supers = RB_INITIALIZER(&g_tstfs_db.supers), \
+};
+
+/** スーパブロックRB木
+ */
+static int _tst_vfs_tstfs_super_cmp(struct _tst_vfs_tstfs_super *_key, 
+				  struct _tst_vfs_tstfs_super *_ent);
+RB_GENERATE_STATIC(_tst_vfs_tstfs_super_tree, _tst_vfs_tstfs_super, ent, 
+		   _tst_vfs_tstfs_super_cmp);
+
+/** I-nodeRB木
+ */
+static int _tst_vfs_tstfs_inode_cmp(struct _tst_vfs_tstfs_inode *_key, 
+				  struct _tst_vfs_tstfs_inode *_ent);
+RB_GENERATE_STATIC(_tst_vfs_tstfs_inode_tree, _tst_vfs_tstfs_inode, ent, 
+		   _tst_vfs_tstfs_inode_cmp);
+
+/** ディレクトリエントリRB木
+ */
+static int _tst_vfs_tstfs_dent_cmp(struct _tst_vfs_tstfs_dent *_key, 
+				  struct _tst_vfs_tstfs_dent *_ent);
+RB_GENERATE_STATIC(_tst_vfs_tstfs_dent_tree, _tst_vfs_tstfs_dent, ent, 
+		   _tst_vfs_tstfs_dent_cmp);
+
+/** データページRB木
+ */
+static int _tst_vfs_tstfs_dpage_cmp(struct _tst_vfs_tstfs_dpage *_key, 
+				  struct _tst_vfs_tstfs_dpage *_ent);
+RB_GENERATE_STATIC(_tst_vfs_tstfs_dpage_tree, _tst_vfs_tstfs_dpage, ent, 
+		   _tst_vfs_tstfs_dpage_cmp);
+
+/**
+   スーパブロック比較関数
+   @param[in] key 比較対象1
+   @param[in] ent RB木内の各エントリ
+   @retval 正  keyが entより前にある
+   @retval 負  keyが entより後にある
+   @retval 0   keyが entに等しい
+ */
+static int
+_tst_vfs_tstfs_super_cmp(struct _tst_vfs_tstfs_super *key, 
+				    struct _tst_vfs_tstfs_super *ent){	
+
+	if ( key->devid < ent->devid )
+		return 1;
+
+	if ( ent->devid < key->devid )
+		return -1;
+
+	return 0;	
+}
+
+/**
+   I-node比較関数
+   @param[in] key 比較対象1
+   @param[in] ent RB木内の各エントリ
+   @retval 正  keyが entより前にある
+   @retval 負  keyが entより後にある
+   @retval 0   keyが entに等しい
+ */
+static int
+_tst_vfs_tstfs_inode_cmp(struct _tst_vfs_tstfs_inode *key, 
+			 struct _tst_vfs_tstfs_inode *ent){	
+
+	if ( key->i_ino < ent->i_ino )
+		return 1;
+
+	if ( ent->i_ino < key->i_ino )
+		return -1;
+
+	return 0;	
+}
+
+/**
+   ディレクトリエントリ比較関数
+   @param[in] key 比較対象1
+   @param[in] ent RB木内の各エントリ
+   @retval 正  keyが entより前にある
+   @retval 負  keyが entより後にある
+   @retval 0   keyが entに等しい
+ */
+static int
+_tst_vfs_tstfs_dent_cmp(struct _tst_vfs_tstfs_dent *key, 
+			struct _tst_vfs_tstfs_dent *ent){	
+
+	if ( strncmp(key->name, ent->name, TST_VFS_TSTFS_FNAME_LEN) > 0 )
+		return 1;
+
+	if ( strncmp(key->name, ent->name, TST_VFS_TSTFS_FNAME_LEN) < 0 )
+		return -1;
+
+	return 0;	
+}
+
+/**
+   データページ比較関数
+   @param[in] key 比較対象1
+   @param[in] ent RB木内の各エントリ
+   @retval 正  keyが entより前にある
+   @retval 負  keyが entより後にある
+   @retval 0   keyが entに等しい
+ */
+static int
+_tst_vfs_tstfs_dpage_cmp(struct _tst_vfs_tstfs_dpage *key, 
+			struct _tst_vfs_tstfs_dpage *ent){	
+
+	if ( key->index < ent->index )
+		return 1;
+
+	if ( ent->index < key->index )
+		return -1;
+
+	return 0;	
+}
 
 void
 tst_vfs_tstfs_init(void){
@@ -40,7 +156,7 @@ tst_vfs_tstfs_init(void){
 	kassert( rc == 0 );
 	
 	/* テスト用ファイルシステムデータページ用SLABキャッシュの初期化 */
-	rc = slab_kmem_cache_create(&tstfs_data, "test file system data", 
-	    sizeof(tst_vfs_tstfs_data), SLAB_ALIGN_NONE,  0, KMALLOC_NORMAL, NULL, NULL);
+	rc = slab_kmem_cache_create(&tstfs_dpage, "test file system data pages", 
+	    sizeof(tst_vfs_tstfs_dpage), SLAB_ALIGN_NONE,  0, KMALLOC_NORMAL, NULL, NULL);
 	kassert( rc == 0 );
 }
--- test/tst-vfs-tstfs.h
+++ test/tst-vfs-tstfs.h
@@ -8,6 +8,9 @@
 #if !defined(_KERN_TST_VFS_TSTFS_H)
 #define _KERN_TST_VFS_TSTFS_H
 
+#include <klib/freestanding.h>
+#include <kern/kern-types.h>
+
 #define TST_VFS_TSTFS_MAGIC (0xfeedfeed)
 #define TST_VFS_TSTFS_VN_MAX    (5)
 #define TST_VFS_TSTFS_ROOT_VNID (2)
@@ -29,16 +32,17 @@ typedef struct _tst_vfs_tstfs_dent{
 /**
    テスト用ファイルシステムデータページ
  */
-typedef struct _tst_vfs_tstfs_data{
-	RB_ENTRY(_tst_vfs_tstfs_data) ent; /**< I-node中のデータページ管理情報のリンク */
-	uintptr_t                  offset; /**< ファイル内オフセットアドレス           */
-	void                        *page; /**< データページ                           */
-}tst_vfs_tstfs_data;
+typedef struct _tst_vfs_tstfs_dpage{
+	RB_ENTRY(_tst_vfs_tstfs_dpage) ent; /**< I-node中のデータページ管理情報のリンク */
+	off_t                        index; /**< オフセットページアドレス               */
+	void                         *page; /**< データページ                           */
+}tst_vfs_tstfs_dpage;
 
 /**
    テスト用ファイルシステムI-node
  */
 typedef struct _tst_vfs_tstfs_inode{
+	uint32_t                      i_ino;  /**< I-node番号                       */
 	uint16_t                     i_mode;  /**< ファイル種別/保護属性            */
 	uint16_t                   i_nlinks;  /**< ファイルのリンク数 (単位:個)     */
 	uint16_t                      i_uid;  /**< ファイル所有者のユーザID         */
@@ -47,7 +51,8 @@ typedef struct _tst_vfs_tstfs_inode{
 	uint32_t                    i_atime;  /**< 最終アクセス時刻 (単位:UNIX時刻) */
 	uint32_t                    i_mtime;  /**< 最終更新時刻 (単位:UNIX時刻)     */
 	uint32_t                    i_ctime;  /**< 最終属性更新時刻 (単位:UNIX時刻) */
-	RB_HEAD(_tst_vfs_tstfs_data_tree, _tst_vfs_tstfs_data) datas;  /**< データページ  */
+	RB_ENTRY(_tst_vfs_tstfs_inode)  ent; /**< スーパブロック中のI-node情報のリンク */
+	RB_HEAD(_tst_vfs_tstfs_dpage_tree, _tst_vfs_tstfs_dpage) datas;  /**< データページ  */
 	/** ディレクトリエントリ  */
 	RB_HEAD(_tst_vfs_tstfs_dent_tree, _tst_vfs_tstfs_dent) dents;  
 }tst_vfs_tstfs_inode;
@@ -56,10 +61,20 @@ typedef struct _tst_vfs_tstfs_inode{
    テスト用ファイルシステムスーパブロック情報
  */
 typedef struct _tst_vfs_tstfs_super{
+	dev_id             devid;  /**< デバイスID                     */
 	uint32_t         s_magic;  /**< スーパブロックのマジック番号   */
 	uint32_t         s_state;  /**< マウント状態                   */
 	BITMAP_TYPE(, uint64_t, TST_VFS_NR_INODES)  inode_map;  /**< I-nodeマップ */
+	RB_ENTRY(_tst_vfs_tstfs_super) ent; /**< ファイルシステムDBへのリンク */
 	RB_HEAD(_tst_vfs_tstfs_inode_tree, _tst_vfs_tstfs_inode) s_inodes; /**< I-node */
 }tst_vfs_tstfs_super;
+
+/**
+   テスト用ファイルシステムDB
+ */
+typedef struct _tst_vfs_tstfs_db{
+	struct _mutex                                         mtx; /**< 排他用ミューテックス */
+	RB_HEAD(_tst_vfs_tstfs_super_tree, _tst_vfs_tstfs_super) supers; /**< スーパブロック */
+}tst_vfs_tstfs_db;
 
 #endif  /*  _KERN_TST_VFS_TSTFS_H  */
