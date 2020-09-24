d43764472f0ba4015f7aa86c9661ac7d28ce15d5
Takeharu KATO <tkato@elliptic-curve.net>
2020-09-25 02:37:40 +0900

    修正概要: ユーザランドテスト用のRTC読み出し処理を追加。

---
+++ hal/uland/x64-rtc.c
@@ -0,0 +1,35 @@
+/* -*- mode: C; coding:utf-8 -*- */
+/**********************************************************************/
+/*  OS kernel sample                                                  */
+/*  Copyright 2019 Takeharu KATO                                      */
+/*                                                                    */
+/*  Pseudo RTC operations                                             */
+/*                                                                    */
+/**********************************************************************/
+
+#include <klib/freestanding.h>
+#include <kern/kern-common.h>
+
+#include <kern/timer.h>
+
+#include <time.h>
+
+/**
+   時刻を読み出す
+   @param[out] ktsp 時刻返却領域
+   @retval   0 正常終了
+   @retval 非0 RTCを読み出せなかった
+ */
+int
+hal_read_rtc(ktimespec *ktsp) {
+	int             rc;
+	struct timespec ts;
+
+	rc = clock_gettime(CLOCK_REALTIME, &ts);  /* 時刻の取得 */
+	kassert( rc == 0 );
+	
+	ktsp->tv_sec = ts.tv_sec;   /* 秒 */
+	ktsp->tv_nsec = ts.tv_nsec; /* ナノ秒 */
+
+	return rc;
+}
