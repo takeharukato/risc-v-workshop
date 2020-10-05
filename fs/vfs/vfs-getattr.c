bc5b338eda95807458db367f9da5c7f17ea72c49
Takeharu KATO <tkato@elliptic-curve.net>
2020-10-05 15:31:21 +0900

    修正概要: 属性情報取得処理を追加。

--- fs/vfs/vfs-getattr.c
+++ fs/vfs/vfs-getattr.c
@@ -14,26 +14,25 @@
 #include <kern/vfs-if.h>
 
 /**
-   ファイルの属性情報を設定する
-   @param[in] v         操作対象ファイルのv-node情報
-   @param[in] stat      設定する属性情報
-   @param[in] stat_mask 設定する属性情報を指示するビットマスク
+   ファイルの属性情報を取得する
+   @param[in]  v         操作対象ファイルのv-node情報
+   @param[in]  stat_mask 取得する属性情報を指示するビットマスク
+   @param[out] statp     属性情報格納先アドレス
    @retval  0      正常終了
    @retval -ENOENT 削除中のv-nodeを指定した
    @retval -EIO    I/Oエラー
    @retval -ENOSYS setattrをサポートしていない 
 */
 int
-vfs_setattr(vnode *v, vfs_file_stat *stat, vfs_vstat_mask stat_mask){
+vfs_getattr(vnode *v, vfs_vstat_mask stat_mask, vfs_file_stat *statp){
 	int                   rc;
 	bool                 res;
 	vfs_vstat_mask attr_mask;
 	vfs_file_stat         st;
 
-	attr_mask = VFS_VSTAT_MASK_SETATTR & stat_mask; /* 設定可能な属性のみを設定 */
+	attr_mask = VFS_VSTAT_MASK_GETATTR & stat_mask; /* 獲得可能な属性のみを抽出 */
 
 	vfs_init_attr_helper(&st); /* 属性情報をクリアする */
-	vfs_copy_attr_helper(&st, stat, attr_mask);  /* 属性情報をコピーする */
 
 	res = vfs_vnode_ref_inc(v);  /* v-nodeへの参照を加算 */
 	if ( !res ) {
@@ -47,7 +46,8 @@ vfs_setattr(vnode *v, vfs_file_stat *stat, vfs_vstat_mask stat_mask){
 	kassert(v->v_mount->m_fs != NULL);
 	kassert( is_valid_fs_calls( v->v_mount->m_fs->c_calls ) );
 
-	if ( v->v_mount->m_fs->c_calls->fs_setattr == NULL ) {
+
+	if ( v->v_mount->m_fs->c_calls->fs_getattr == NULL ) {
 
 		rc = -ENOSYS;  /*  ハンドラが定義されていない場合は, -ENOSYSを返却して復帰  */
 		goto unref_vnode_out;
@@ -55,13 +55,15 @@ vfs_setattr(vnode *v, vfs_file_stat *stat, vfs_vstat_mask stat_mask){
 
 	/* ファイル属性情報を設定する
 	 */
-	rc = v->v_mount->m_fs->c_calls->fs_setattr(
-		v->v_mount->m_fs_super, v->v_id, 
-		v->v_fs_vnode, &st, attr_mask);
+	rc = v->v_mount->m_fs->c_calls->fs_getattr(v->v_mount->m_fs_super, 
+						   v->v_fs_vnode, attr_mask, &st);
 	if ( rc != 0 ) 
 		goto unref_vnode_out;  /* エラー復帰する */
 
 	vfs_vnode_ref_dec(v);  /* v-nodeへの参照を減算 */
+
+	if ( statp != NULL )
+		vfs_copy_attr_helper(statp, &st, attr_mask);  /* 属性情報をコピーする */
 
 	return 0;
 
