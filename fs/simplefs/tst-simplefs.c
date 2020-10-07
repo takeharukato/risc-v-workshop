bdc373d4eae5df034a37619507635f56be06f1b7
Takeharu KATO <tkato@elliptic-curve.net>
2020-10-07 22:30:14 +0900

    修正概要: rmdirのテスト

--- fs/simplefs/tst-simplefs.c
+++ fs/simplefs/tst-simplefs.c
@@ -170,13 +170,29 @@ simplefs2(struct _ktest_stats *sp, void __unused *arg){
 		ktest_pass( sp );
 	else
 		ktest_fail( sp );
+	rc = vfs_getdents(tst_ioctx.cur, fd, &buf[0], 0, 1024, &nread);
+	kprintf("%8s %-10s %s %10s %s\n", "I-num", "type", "reclen", "off", "name");
+	for (bpos = 0;  nread > bpos; bpos += d->d_reclen) {
+
+		d = (vfs_dirent *) (buf + bpos);
+		kprintf("%8ld  ", d->d_ino);
+		d_type = *(buf + bpos + d->d_reclen - 1);
+		kprintf("%-10s ", (d_type == DT_REG) ?  "regular" :
+		    (d_type == DT_DIR) ?  "directory" :
+		    (d_type == DT_FIFO) ? "FIFO" :
+		    (d_type == DT_SOCK) ? "socket" :
+		    (d_type == DT_LNK) ?  "symlink" :
+		    (d_type == DT_BLK) ?  "block dev" :
+		    (d_type == DT_CHR) ?  "char dev" : "???");
+		kprintf("%4d %10lld  %s\n", d->d_reclen,
+		    (long long) d->d_off, d->d_name);
+	}
 	/* 単純なファイルシステムをアンマウントする */
 	rc = vfs_unmount(tst_ioctx.cur, "/dev");
 	if ( rc == 0 )
 		ktest_pass( sp );
 	else
 		ktest_fail( sp );
-
 
 	rc = vfs_getdents(tst_ioctx.cur, fd, &buf[0], 0, 1024, &nread);
 	kprintf("%8s %-10s %s %10s %s\n", "I-num", "type", "reclen", "off", "name");
@@ -195,6 +211,14 @@ simplefs2(struct _ktest_stats *sp, void __unused *arg){
 		kprintf("%4d %10lld  %s\n", d->d_reclen,
 		    (long long) d->d_off, d->d_name);
 	}
+	/* ディレクトリ削除
+	 */
+	rc = vfs_rmdir(tst_ioctx.cur, "/dev");
+	if ( rc == 0 )
+		ktest_pass( sp );
+	else
+		ktest_fail( sp );
+
 }
 
 /**
